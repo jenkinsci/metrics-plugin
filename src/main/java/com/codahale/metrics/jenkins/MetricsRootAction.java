76c08c3470922c3218a865f65d2f047f9cfd45e2
Stephen Connolly <stephen.alan.connolly@gmail.com>
2014-02-17 15:55:56 +0000

    Add POST based queries that don't expose the Key in the URL logs of requests

--- src/main/java/com/codahale/metrics/jenkins/MetricsRootAction.java
+++ src/main/java/com/codahale/metrics/jenkins/MetricsRootAction.java
@@ -24,6 +24,7 @@
 
 package com.codahale.metrics.jenkins;
 
+import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.json.HealthCheckModule;
 import com.codahale.metrics.json.MetricsModule;
@@ -34,8 +35,10 @@ import hudson.Extension;
 import hudson.model.UnprotectedRootAction;
 import hudson.util.HttpResponses;
 import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -59,6 +62,23 @@ public class MetricsRootAction implements UnprotectedRootAction {
     private final ObjectMapper metricsMapper =
             new ObjectMapper().registerModule(new MetricsModule(TimeUnit.MINUTES, TimeUnit.SECONDS, true));
 
+    private static boolean isAllHealthy(Map<String, HealthCheck.Result> results) {
+        for (HealthCheck.Result result : results.values()) {
+            if (!result.isHealthy()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static ObjectWriter getWriter(ObjectMapper mapper, HttpServletRequest request) {
+        final boolean prettyPrint = Boolean.parseBoolean(request.getParameter("pretty"));
+        if (prettyPrint) {
+            return mapper.writerWithDefaultPrettyPrinter();
+        }
+        return mapper.writer();
+    }
+
     public String getIconFileName() {
         return null;
     }
@@ -76,120 +96,46 @@ public class MetricsRootAction implements UnprotectedRootAction {
         return admin;
     }
 
+    @RequirePOST
+    public HttpResponse doHealthcheck(@QueryParameter("key") String key) {
+        Metrics.checkAccessKey(key);
+        return new HealthCheckResponse(Metrics.healthCheckRegistry().runHealthChecks());
+    }
+
+    @RequirePOST
+    public HttpResponse doMetrics(@QueryParameter("key") String key) {
+        Metrics.checkAccessKey(key);
+        return new MetricsResponse(Metrics.metricRegistry());
+    }
+
+    @RequirePOST
+    public HttpResponse doPing(@QueryParameter("key") String key) {
+        Metrics.checkAccessKey(key);
+        return new PingResponse();
+    }
+
+    @RequirePOST
+    public HttpResponse doThreads(@QueryParameter("key") String key) {
+        Metrics.checkAccessKey(key);
+        return new ThreadDumpResponse(new ThreadDump(ManagementFactory.getThreadMXBean()));
+    }
+
     public class Admin {
 
-        private boolean isAllHealthy(Map<String, HealthCheck.Result> results) {
-            for (HealthCheck.Result result : results.values()) {
-                if (!result.isHealthy()) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private ObjectWriter getWriter(ObjectMapper mapper, HttpServletRequest request) {
-            final boolean prettyPrint = Boolean.parseBoolean(request.getParameter("pretty"));
-            if (prettyPrint) {
-                return mapper.writerWithDefaultPrettyPrinter();
-            }
-            return mapper.writer();
-        }
-
         public HttpResponse doHealthcheck() {
-            final SortedMap<String, HealthCheck.Result> results = Metrics.healthCheckRegistry().runHealthChecks();
-            return new HttpResponse() {
-                private static final String CONTENT_TYPE = "application/json";
-                private static final String CACHE_CONTROL = "Cache-Control";
-                private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
-
-                public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
-                        ServletException {
-                    resp.setHeader(CACHE_CONTROL, NO_CACHE);
-                    resp.setContentType(CONTENT_TYPE);
-                    if (results.isEmpty()) {
-                        resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
-                    } else {
-                        if (isAllHealthy(results)) {
-                            resp.setStatus(HttpServletResponse.SC_OK);
-                        } else {
-                            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                        }
-                    }
-
-                    final OutputStream output = resp.getOutputStream();
-                    try {
-                        getWriter(healthCheckMapper, req).writeValue(output, results);
-                    } finally {
-                        output.close();
-                    }
-                }
-            };
+            return new HealthCheckResponse(Metrics.healthCheckRegistry().runHealthChecks());
         }
 
         public HttpResponse doMetrics() {
-            return new HttpResponse() {
-                private static final String CONTENT_TYPE = "application/json";
-                private static final String CACHE_CONTROL = "Cache-Control";
-                private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
-
-                public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
-                        ServletException {
-                    resp.setStatus(HttpServletResponse.SC_OK);
-                    resp.setHeader(CACHE_CONTROL, NO_CACHE);
-                    resp.setContentType(CONTENT_TYPE);
-
-                    final OutputStream output = resp.getOutputStream();
-                    try {
-                        getWriter(healthCheckMapper, req).writeValue(output, Metrics.metricRegistry());
-                    } finally {
-                        output.close();
-                    }
-                }
-            };
+            return new MetricsResponse(Metrics.metricRegistry());
         }
 
         public HttpResponse doPing() {
-            return new HttpResponse() {
-                private static final String CONTENT_TYPE = "text/plain";
-                private static final String CONTENT = "pong";
-                private static final String CACHE_CONTROL = "Cache-Control";
-                private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
-
-                public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
-                        ServletException {
-                    resp.setStatus(HttpServletResponse.SC_OK);
-                    resp.setHeader(CACHE_CONTROL, NO_CACHE);
-                    resp.setContentType(CONTENT_TYPE);
-                    final PrintWriter writer = resp.getWriter();
-                    try {
-                        writer.println(CONTENT);
-                    } finally {
-                        writer.close();
-                    }
-                }
-            };
+            return new PingResponse();
         }
 
         public HttpResponse doThreads() {
-            final ThreadDump threadDump = new ThreadDump(ManagementFactory.getThreadMXBean());
-            return new HttpResponse() {
-                private static final String CONTENT_TYPE = "text/plain";
-                private static final String CACHE_CONTROL = "Cache-Control";
-                private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
-
-                public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
-                        ServletException {
-                    resp.setStatus(HttpServletResponse.SC_OK);
-                    resp.setHeader(CACHE_CONTROL, NO_CACHE);
-                    resp.setContentType(CONTENT_TYPE);
-                    final OutputStream output = resp.getOutputStream();
-                    try {
-                        threadDump.dump(output);
-                    } finally {
-                        output.close();
-                    }
-                }
-            };
+            return new ThreadDumpResponse(new ThreadDump(ManagementFactory.getThreadMXBean()));
         }
 
         public HttpResponse doIndex() {
@@ -211,6 +157,108 @@ public class MetricsRootAction implements UnprotectedRootAction {
                             "</body>\n" +
                             "</html>");
         }
+
     }
 
+    private class HealthCheckResponse implements HttpResponse {
+        private static final String CONTENT_TYPE = "application/json";
+        private static final String CACHE_CONTROL = "Cache-Control";
+        private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
+        private final SortedMap<String, HealthCheck.Result> results;
+
+        public HealthCheckResponse(SortedMap<String, HealthCheck.Result> results) {
+            this.results = results;
+        }
+
+        public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
+                ServletException {
+            resp.setHeader(CACHE_CONTROL, NO_CACHE);
+            resp.setContentType(CONTENT_TYPE);
+            if (results.isEmpty()) {
+                resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+            } else {
+                if (isAllHealthy(results)) {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }
+            }
+
+            final OutputStream output = resp.getOutputStream();
+            try {
+                getWriter(healthCheckMapper, req).writeValue(output, results);
+            } finally {
+                output.close();
+            }
+        }
+    }
+
+    private class MetricsResponse implements HttpResponse {
+        private static final String CONTENT_TYPE = "application/json";
+        private static final String CACHE_CONTROL = "Cache-Control";
+        private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
+        private final MetricRegistry registry;
+
+        private MetricsResponse(MetricRegistry registry) {
+            this.registry = registry;
+        }
+
+        public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
+                ServletException {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setHeader(CACHE_CONTROL, NO_CACHE);
+            resp.setContentType(CONTENT_TYPE);
+
+            final OutputStream output = resp.getOutputStream();
+            try {
+                getWriter(metricsMapper, req).writeValue(output, registry);
+            } finally {
+                output.close();
+            }
+        }
+    }
+
+    private class PingResponse implements HttpResponse {
+        private static final String CONTENT_TYPE = "text/plain";
+        private static final String CONTENT = "pong";
+        private static final String CACHE_CONTROL = "Cache-Control";
+        private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
+
+        public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
+                ServletException {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setHeader(CACHE_CONTROL, NO_CACHE);
+            resp.setContentType(CONTENT_TYPE);
+            final PrintWriter writer = resp.getWriter();
+            try {
+                writer.println(CONTENT);
+            } finally {
+                writer.close();
+            }
+        }
+    }
+
+    private class ThreadDumpResponse implements HttpResponse {
+        private static final String CONTENT_TYPE = "text/plain";
+        private static final String CACHE_CONTROL = "Cache-Control";
+        private static final String NO_CACHE = "must-revalidate,no-cache,no-store";
+        private final ThreadDump threadDump;
+
+        public ThreadDumpResponse(ThreadDump threadDump) {
+            this.threadDump = threadDump;
+        }
+
+        public void generateResponse(StaplerRequest req, StaplerResponse resp, Object node) throws IOException,
+                ServletException {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setHeader(CACHE_CONTROL, NO_CACHE);
+            resp.setContentType(CONTENT_TYPE);
+            final OutputStream output = resp.getOutputStream();
+            try {
+                threadDump.dump(output);
+            } finally {
+                output.close();
+            }
+        }
+    }
 }
