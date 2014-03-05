48f6c531abf8ebf02871c06d9535b8f58fb9e04e
Stephen Connolly <stephen.alan.connolly@gmail.com>
2014-03-05 17:26:10 +0000

    Add metrics for the system and jvm cpu load

--- src/main/java/com/codahale/metrics/jenkins/impl/VMMetricProviderImpl.java
+++ src/main/java/com/codahale/metrics/jenkins/impl/VMMetricProviderImpl.java
@@ -24,7 +24,8 @@
 
 package com.codahale.metrics.jenkins.impl;
 
-import com.codahale.metrics.Metric;
+import com.codahale.metrics.CachedGauge;
+import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.jenkins.MetricProvider;
@@ -32,35 +33,129 @@ import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Stephen Connolly
  */
 @Extension
 public class VMMetricProviderImpl extends MetricProvider {
-    private final Map<String, Metric> map;
-    private final MetricSet set = new MetricSet() {
-        public Map<String, Metric> getMetrics() {
-            return map;
-        }
-    };
+    private final MetricSet set;
+    private final Gauge<Double> systemCpuLoad;
+    private final Gauge<Double> vmCpuLoad;
 
     public VMMetricProviderImpl() {
-        map = new LinkedHashMap<String, Metric>();
-        map.put(MetricRegistry.name("vm", "memory"), new MemoryUsageGaugeSet());
-        map.put(MetricRegistry.name("vm", "gc"), new GarbageCollectorMetricSet());
-        map.put(MetricRegistry.name("vm", "file.descriptor.ratio"), new FileDescriptorRatioGauge());
-        map.put(MetricRegistry.name("vm"), new ThreadStatesGaugeSet());
+        Gauge<Double> systemLoad = new
+                Gauge<Double>() {
+                    public Double getValue() {
+                        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+                        return operatingSystemMXBean.getSystemLoadAverage();
+                    }
+                };
+        Gauge<Double> cpuUsage;
+        try {
+            cpuUsage = new CpuUsageGauge(1, TimeUnit.SECONDS);
+        } catch (UnsupportedOperationException e) {
+            // not supported
+            cpuUsage = null;
+        }
+        systemCpuLoad = systemLoad.getValue() >= 0 ? systemLoad : null;
+        vmCpuLoad = cpuUsage;
+        set = metrics(
+                metric(MetricRegistry.name("vm", "memory"), new MemoryUsageGaugeSet()),
+                metric(MetricRegistry.name("vm", "gc"), new GarbageCollectorMetricSet()),
+                metric(MetricRegistry.name("vm", "file.descriptor.ratio"), new FileDescriptorRatioGauge()),
+                metric(MetricRegistry.name("vm"), new ThreadStatesGaugeSet()),
+                metric(MetricRegistry.name("vm", "uptime", "milliseconds"), new Gauge<Long>() {
+                    public Long getValue() {
+                        return ManagementFactory.getRuntimeMXBean().getUptime();
+                    }
+                }),
+                metric(MetricRegistry.name("system", "cpu", "load"), systemCpuLoad),
+                metric(MetricRegistry.name("vm", "cpu", "load"), vmCpuLoad)
+        );
+    }
+
+    /**
+     * Returns a gauge that reports the current system CPU load or {@code null} if that metric is unavailable.
+     *
+     * @return a gauge that reports the current system CPU load or {@code null} if that metric is unavailable.
+     */
+    @CheckForNull
+    public Gauge<Double> getSystemCpuLoad() {
+        return systemCpuLoad;
+    }
+
+    /**
+     * Returns a gauge that reports the current JVM CPU load or {@code null} if that metric is unavailable.
+     *
+     * @return a gauge that reports the current JVM CPU load or {@code null} if that metric is unavailable.
+     */
+    @CheckForNull
+    public Gauge<Double> getVmCpuLoad() {
+        return vmCpuLoad;
     }
 
     @NonNull
     @Override
     public MetricSet getMetricSet() {
         return set;
+    }
+
+    private static class CpuUsageGauge extends CachedGauge<Double> {
+        private final RuntimeMXBean runtimeMXBean;
+        private final Method getProcessCpuTime;
+        private final OperatingSystemMXBean operatingSystemMXBean;
+        long prevUptime;
+        long prevCpu;
+
+        public CpuUsageGauge(int timeout, TimeUnit timeUnit) {
+            super(timeout, timeUnit);
+            runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+            operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+            try {
+                getProcessCpuTime = operatingSystemMXBean.getClass().getMethod("getProcessCpuTime");
+                getProcessCpuTime.setAccessible(true);
+                if (((Number) getProcessCpuTime.invoke(operatingSystemMXBean)).longValue() < 0) {
+                    throw new UnsupportedOperationException("CPU usage not supported");
+                }
+            } catch (ClassCastException e) {
+                throw new UnsupportedOperationException("CPU usage not supported", e);
+            } catch (InvocationTargetException e) {
+                throw new UnsupportedOperationException("CPU usage not supported", e);
+            } catch (IllegalAccessException e) {
+                throw new UnsupportedOperationException("CPU usage not supported", e);
+            } catch (NoSuchMethodException e) {
+                throw new UnsupportedOperationException("CPU usage not supported", e);
+            }
+        }
+
+        @Override
+        protected synchronized Double loadValue() {
+            try {
+                long uptime = runtimeMXBean.getUptime();
+                long cpu = ((Number) getProcessCpuTime.invoke(operatingSystemMXBean)).longValue();
+                long elapsedTime = uptime - prevUptime;
+                double elapsedCpu = TimeUnit.NANOSECONDS.toMillis(cpu - prevCpu);
+                prevUptime = uptime;
+                prevCpu = cpu;
+                return Math.min(99.0, elapsedCpu / elapsedTime);
+            } catch (IllegalAccessException e) {
+                // should never happen as pre-flight test caught it
+                return -1.0;
+            } catch (InvocationTargetException e) {
+                // should never happen as pre-flight test caught it
+                return -1.0;
+            }
+        }
     }
 }
