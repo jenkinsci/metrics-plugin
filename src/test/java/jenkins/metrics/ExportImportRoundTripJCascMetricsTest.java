ad614e8a2518e805a7e1e4ebc1480ba56fbe038b
Ramon Leon <manuelramonleonjimenez@gmail.com>
2019-06-03 08:45:25 +0200

    Revert "[Jenkins-57761] Move the abstract test to the JCasC plugin"
    This reverts commit 04210250127ea90fc9bbd0d5e8db539b6aedf96c.

--- src/test/java/jenkins/metrics/ExportImportRoundTripJCascMetricsTest.java
+++ src/test/java/jenkins/metrics/ExportImportRoundTripJCascMetricsTest.java
@@ -1,7 +1,6 @@
 package jenkins.metrics;
 
 import hudson.ExtensionList;
-import io.jenkins.plugins.casc.misc.ExportImportRoundTripAbstractTest;
 import jenkins.metrics.api.MetricsAccessKey;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
 
