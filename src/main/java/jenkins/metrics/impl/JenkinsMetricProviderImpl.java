b1318d90bcdfaf983e3b0b114618f60f94098702
Jose Blas Camacho Taboada <joseblas@gmail.com>
2020-11-09 10:23:00 +0100

    change JEnkinsfile to remove recommended config

--- src/main/java/jenkins/metrics/impl/JenkinsMetricProviderImpl.java
+++ src/main/java/jenkins/metrics/impl/JenkinsMetricProviderImpl.java
@@ -1018,7 +1018,7 @@ public class JenkinsMetricProviderImpl extends MetricProvider {
                         null,
                         null);
                 executorService.submit(() -> {
-                     QueueItemMetricsListener.notifyQueued(m);
+                    QueueItemMetricsListener.notifyQueued(m);
                 });
                 return new ItemTotals(id);
             });
